5dff30a5e6678a24af2e993e3741281b68419a4b
Agana-Joseph <josephagana8@gmail.com>
2024-07-10 04:39:25 +0100

    initail template setup-(Yet to configure postcss)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+//components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
+//pages
+
+
+// react router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/room/:id',
+    element: <RoomDetails />,
+  },
+  {
+    path: '/restaurants',
+    element: <Restaurant />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+])
+
+
+
+
+const App = () => {
+  return <div>
+    <Header />
+    <RouterProvider router={router} />
+    <Service />
+  <Special />
+    <Footer />
+  </div>;
+};
 
 export default App;
